8913a0fde812a59bcee6b4c9c2c6d151f90233f2
hhai0410 <nguyenhoangghaii0410@gmail.com>
2026-08-06 23:36:34 +0700

    Combined L1 and L2, some Kestra syntax

---
+++ lesson2/pipeline/ingest_data.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--target-table', default='yellow_taxi_data',
+              help='Target table')
+@click.option('--year', required=True, type=int,
+              help='Dataset year')
+@click.option('--month', required=True, type=int,
+              help='Dataset month')
+@click.option('--chunksize', default=100000, type=int,
+              help='Chunk size')
+def run(pg_user, pg_pass, pg_host, pg_port,
+        pg_db, target_table,
+        year, month, chunksize):
+
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
+
+    print(f"Reading data from: {url}")
+
+    engine = create_engine(
+        f"postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    )
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="replace",
+                index=False
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+
+    print("========== IMPORT FINISHED ==========")
+
+
+if __name__ == "__main__":
+    run()
